b94620d85388b7cb3dc394bdcf458163654fca06
Corey Davis <coreyjamesdavis@gmail.com>
2015-04-16 21:26:44 -0400

    Creation of Defining Variables
    declaration and definition on separate lines or declare and define on
    same line.

---
+++ Video Assignments/Variables_Defining/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by CoreyDavis on 4/16/15.
+ * MDV 2330
+ *
+ */
+
+//Defining Variables
+
+//variables
+var age; //declaration
+age = 38;  //definition
+//storage container = value
+
+var yearOfBirth=1970; //declaring and defining on the same line
+//prompts
+
+
+//outputs
