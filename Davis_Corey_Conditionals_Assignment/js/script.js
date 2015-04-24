179d665d36ebebf67b248311dff3443752d71f25
Corey Davis <coreyjamesdavis@gmail.com>
2015-04-23 22:11:44 -0400

    Variable Declarations
    variables needed to calculate old phone cost to upgrade to new phone

--- Davis_Corey_Conditionals_Assignment/js/script.js
+++ Davis_Corey_Conditionals_Assignment/js/script.js
@@ -3,4 +3,17 @@
  * SDI MDV 2330
  * Conditionals Assignment
  */
-alert("Javascript is functional!");
+//alert("Javascript is functional!");
+
+//variable declarations
+var oldPhone;  //user prompt variable
+var newPhone;  //user prompt variable
+var paymentsMade;  //user prompt variable
+var remainBal;  //store calculation of old phone cost - payments made
+var newCost;  //store calculation of new phone cost - payments made
+var oldUpgrade;  //store calculation of 50% of old phone cost
+var upgradeEligible;  //store calculation of payment needed to reach 50% of old phone cost
+var monthlyCost;  //store remaining cost of new phone over 12 months
+
+
+
