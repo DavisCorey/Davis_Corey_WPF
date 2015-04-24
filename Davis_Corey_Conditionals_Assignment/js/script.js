ded0b5109750e7cc79d1a7d21dd3af62554dddbf
Corey Davis <coreyjamesdavis@gmail.com>
2015-04-23 22:23:16 -0400

    User prompts
    Created user prompts for calculation of upgrade for new smartphone.

--- Davis_Corey_Conditionals_Assignment/js/script.js
+++ Davis_Corey_Conditionals_Assignment/js/script.js
@@ -15,5 +15,10 @@ var oldUpgrade;  //store calculation of 50% of old phone cost
 var upgradeEligible;  //store calculation of payment needed to reach 50% of old phone cost
 var monthlyCost;  //store remaining cost of new phone over 12 months
 
+//user prompts
+oldPhone=Number(prompt("Let's calculate how much it cost to upgrade to a new smartphone. \n Please input" +
+    " the total cost of the current smartphone."));
+newPhone=Number(prompt("How much is the new smartphone's retail cost?"));
+paymentsMade=Number(prompt("Please input the total cost of payments made for the current smartphone."));
 
 
