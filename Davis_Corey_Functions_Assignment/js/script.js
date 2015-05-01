e09616bf7c6bb1ca3d2a8f86e7d87dd36af60037
Corey Davis <coreyjamesdavis@gmail.com>
2015-04-30 23:27:43 -0400

    3rd User Prompt Weekly Production
    While loop used to verify user input.

--- Davis_Corey_Functions_Assignment/js/script.js
+++ Davis_Corey_Functions_Assignment/js/script.js
@@ -19,16 +19,29 @@ while(makeProduct===""||isNAN(makeProduct)){
         makeProduct=prompt("Please only use numbers, \n can you enter the cost of each product to make each:");
     }
 }
-//Prompt user for cost to make product
+//Prompt user for cost to sell product or service
 var sellProduct=prompt("Please enter the expected retail cost of the product or service individually:");
 //Parse returned value to a number
 sellProduct=parseInt(sellProduct);
 //Prompt Validation
 while(sellProduct===""||isNAN(sellProduct)){
-    //Re-prompt user 
+    //Re-prompt user
     if(sellProduct===""){  //Blank
         sellProduct=prompt("Please do not leave blank, \n what is the retail value for each product:");
     }else if(isNaN){  //Not a number
         sellProduct=prompt("Please only use numbers, \n can you please retail cost for each product:");
+    }
+}
+//Prompt user for units manufactured per week
+var weekProduction=prompt("Please enter the expected  product that can be produced per week:");
+//Parse returned value to a number
+weekProduction=parseInt(weekProduction);
+//Prompt Validation
+while(weekProduction===""||isNAN(weekProduction)){
+    //Re-prompt user
+    if(weekProduction===""){  //Blank
+        weekProduction=prompt("Please do not leave blank, \n what is the number of products promised per week:");
+    }else if(isNaN){  //Not a number
+        weekProduction=prompt("Please only use numbers, \n can you please input the number of product produced weekly:");
     }
 }
