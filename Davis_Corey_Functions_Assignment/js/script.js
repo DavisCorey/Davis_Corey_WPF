fe19f7a48b4e1147a68674c460056c6d3347c8f1
Corey Davis <coreyjamesdavis@gmail.com>
2015-04-30 23:19:57 -0400

    2nd User Prompt
    prompt user, while statement used to verify user input

--- Davis_Corey_Functions_Assignment/js/script.js
+++ Davis_Corey_Functions_Assignment/js/script.js
@@ -19,3 +19,16 @@ while(makeProduct===""||isNAN(makeProduct)){
         makeProduct=prompt("Please only use numbers, \n can you enter the cost of each product to make each:");
     }
 }
+//Prompt user for cost to make product
+var sellProduct=prompt("Please enter the expected retail cost of the product or service individually:");
+//Parse returned value to a number
+sellProduct=parseInt(sellProduct);
+//Prompt Validation
+while(sellProduct===""||isNAN(sellProduct)){
+    //Re-prompt user 
+    if(sellProduct===""){  //Blank
+        sellProduct=prompt("Please do not leave blank, \n what is the retail value for each product:");
+    }else if(isNaN){  //Not a number
+        sellProduct=prompt("Please only use numbers, \n can you please retail cost for each product:");
+    }
+}
