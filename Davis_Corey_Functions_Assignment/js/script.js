b5121eb98bc6faa321db686c90314c6ed08de543
Corey Davis <coreyjamesdavis@gmail.com>
2015-05-01 00:50:49 -0400

    Unresolved error with anonoymous functions

--- Davis_Corey_Functions_Assignment/js/script.js
+++ Davis_Corey_Functions_Assignment/js/script.js
@@ -5,13 +5,17 @@
  */
 //alert("Javascript Functional!");
 
+
 //Prompt user for cost to make product
 var makeProduct=prompt("To evaluate the maximum profits for this start-up business" +
     ",\n please enter the cost to produce each product individually:");
 //Parse returned value to a number
 makeProduct=parseInt(makeProduct);
 //Prompt Validation
-while(makeProduct===""||isNAN(makeProduct)){
+var isNAN = function (makeProduct,sellProduct,weekProduction) {
+    
+};
+while(makeProduct===""|| isNAN(makeProduct)){
     //Re-prompt User
     if(makeProduct===""){  //Blank
         makeProduct=prompt("Please do not leave blank, \n what is the cost of each product to make each:");
@@ -26,7 +30,7 @@ sellProduct=parseInt(sellProduct);
 //Prompt Validation
 while(sellProduct===""||isNAN(sellProduct)){
     //Re-prompt user
-    if(sellProduct===""){  //Blank
+    if(sellProduct===""){  //Blank string
         sellProduct=prompt("Please do not leave blank, \n what is the retail value for each product:");
     }else if(isNaN){  //Not a number
         sellProduct=prompt("Please only use numbers, \n can you please retail cost for each product:");
@@ -39,9 +43,41 @@ weekProduction=parseInt(weekProduction);
 //Prompt Validation
 while(weekProduction===""||isNAN(weekProduction)){
     //Re-prompt user
-    if(weekProduction===""){  //Blank
+    if(weekProduction===""){  //Blank string
         weekProduction=prompt("Please do not leave blank, \n what is the number of products promised per week:");
     }else if(isNaN){  //Not a number
         weekProduction=prompt("Please only use numbers, \n can you please input the number of product produced weekly:");
     }
 }
+//Anonymous Function for initial calculation of cost for a week supply of inventory
+var startupCost;
+startupCost = function () {
+    return cost * inventory
+};
+
+//Function call
+var total;
+total= startupcost(makeProduct, weekProduction); {
+    
+}
+ //Display of week of production cost
+console.log("One week of manufactured products will cost"+initialCost);
+
+var totaloutput=returnedInvestment(makeProduct,sellProduct,weekProduction);
+console.log("To produce this product for "+makeProduct+" dollars and will make "+totaloutput+" dollars in profits." )
+
+//Function to evaluate profits
+function returnedInvestment(num1,num2,num3){
+    //Calculate
+    var totalProfits= (num2*num3)-(num1 * num3);
+            //return value
+    return totalProfits;
+}
+
+
+/*Test values entered
+Product cost to make 7
+Product cost to sell  10
+Product manufactured per week 500
+
+ */
